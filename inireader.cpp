481fa2e59710a59e8c347479c44424459056b178
blingu <36486731+blingu@users.noreply.github.com>
2019-09-01 17:17:40 +0200

    Update inireader.cpp

--- inireader.cpp
+++ inireader.cpp
@@ -7,35 +7,35 @@ char pathbuffer[256];
 
 char get_path(char *inibuffer)
 {
-		GetCurrentDirectory(sizeof(pathbuffer), pathbuffer);
-		strncat(pathbuffer, "\\", 2);
-		strncat(pathbuffer, inibuffer, strlen(inibuffer));
+	GetCurrentDirectory(sizeof(pathbuffer), pathbuffer);
+	strncat(pathbuffer, "\\", 2);
+	strncat(pathbuffer, inibuffer, strlen(inibuffer));
 
-		return file_exists(pathbuffer);
+	return file_exists(pathbuffer);
 }
 
 int read_var(const char *str)
 {
-		char linebuffer[256];
-		FILE *fd = fopen(pathbuffer, "r");
+	char linebuffer[256];
+	FILE *fd = fopen(pathbuffer, "r");
 
-		if (fd) {
-			char option[32];
-			char valbuf[32];
-			int value;
+	if (fd) {
+		char option[32];
+		char valbuf[32];
+		int value;
 
-			while (fgets(linebuffer, sizeof(linebuffer), fd) != NULL) {
-				if (linebuffer[0] == '#' || linebuffer[0] == '[' || linebuffer[0] == '\n')
-					continue;
+		while (fgets(linebuffer, sizeof(linebuffer), fd) != NULL) {
+			if (linebuffer[0] == '#' || linebuffer[0] == '[' || linebuffer[0] == '\n')
+				continue;
 
-					sscanf(linebuffer, "%s = %s", option, &valbuf);
-					value = strtol(valbuf, NULL, 0);
+				sscanf(linebuffer, "%s = %s", option, &valbuf);
+				value = strtol(valbuf, NULL, 0);
 
-					if (strcmp(option, str) == 0) {
-						fclose(fd);
-						return value;
+				if (strcmp(option, str) == 0) {
+					fclose(fd);
+					return value;
 				}
-			}
 		}
-		return -1;
+	}
+	return -1;
 }
